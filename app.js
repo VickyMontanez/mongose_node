4375e0832fabb308e6c9703d8555beaf9865249b
VickyMontanezCampus <vmontanez707@gmail.com>
2023-08-04 08:54:21 -0500

    feat: :sparkles: New method of List all collections
    New method for List all collections of the database from node

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import { conx } from "./db/atlas.js";
 
 dotenv.config();
 const app = express();
@@ -10,3 +11,10 @@ const config = JSON.parse(process.env.MY_SERVER);
 app.listen(config,()=>{
     console.log(`http://${config.hostname}:${config.port}`)
 });
+
+
+let db = await conx();
+
+//List all collections
+const collections = await db.listCollections().toArray();
+console.log(collections);
